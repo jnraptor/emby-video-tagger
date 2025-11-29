83761d5bbee50d697fddf5f7c4cb66e1a75f6003
jnraptor <jnraptor@gmail.com>
2025-11-29 23:41:18 +0800

    Update nvidia cuda image and llama.cpp branch

--- llama-serve-modal.py
+++ llama-serve-modal.py
@@ -18,25 +18,26 @@
 # OpenAI-compatible client library or tools like `curl` to interact with the model.
 # The server exposes standard endpoints like `/v1/chat/completions`.
 
-import modal
-from pathlib import Path
 import subprocess
+from pathlib import Path
+
+import modal
 
 # --- Variables ---
-#FILENAME = "Qwen2.5-VL-7B-NSFW-Caption-V4.Q8_0.gguf"
-#MPROJ_FILENAME = "Qwen2.5-VL-7B-NSFW-Caption-V4.mmproj-Q8_0.gguf"
+# FILENAME = "Qwen2.5-VL-7B-NSFW-Caption-V4.Q8_0.gguf"
+# MPROJ_FILENAME = "Qwen2.5-VL-7B-NSFW-Caption-V4.mmproj-Q8_0.gguf"
 FILENAME = "Qwen3-VL-8B-NSFW-Caption-V4.5.Q8_0.gguf"
 MPROJ_FILENAME = "Qwen3-VL-8B-NSFW-Caption-V4.5.mmproj-Q8_0.gguf"
 ALIAS = "InternVL3_5-1B"
 N_GPU_LAYERS = "99"
-CTX_SIZE = "12288" # 4096*3
+CTX_SIZE = "12288"  # 4096*3
 BATCH = "2048"
 UBATCH = "2048"
 PARALLEL = "3"
 API_KEY = "ByU8dGHlt8chOIKT"
-#TAG = "12.9.1-devel-ubuntu22.04"
-TAG = "13.0.1-devel-ubuntu24.04"
-GPU = "L4" # T4, L4, A10 Available GPUs: https://modal.com/pricing, https://modal.com/docs/guide/gpu#specifying-gpu-type
+# TAG = "12.9.1-devel-ubuntu22.04"
+TAG = "13.0.2-devel-ubuntu24.04"
+GPU = "L4"  # T4, L4, A10 Available GPUs: https://modal.com/pricing, https://modal.com/docs/guide/gpu#specifying-gpu-type
 
 # --- Configuration ---
 # Define the base Docker image from ghcr.io, a persistent volume for models,
@@ -44,21 +45,23 @@ GPU = "L4" # T4, L4, A10 Available GPUs: https://modal.com/pricing, https://moda
 model_volume = modal.Volume.from_name("llama-models-store", create_if_missing=True)
 MODEL_DIR = "/models"
 llama_image = (
-    #modal.Image.from_registry("ghcr.io/ggml-org/llama.cpp:server-cuda", add_python="3.11")
+    # modal.Image.from_registry("ghcr.io/ggml-org/llama.cpp:server-cuda", add_python="3.11")
     modal.Image.from_registry(f"nvidia/cuda:{TAG}", add_python="3.12")
-    .apt_install("git", "build-essential", "cmake", "curl", "libcurl4-openssl-dev", "ccache")
-    .run_commands("git clone --depth 1 --branch b6992 https://github.com/ggml-org/llama.cpp", force_build=False)
-    .run_commands(
-        "nvidia-smi",
-        gpu=GPU
+    .apt_install(
+        "git", "build-essential", "cmake", "curl", "libcurl4-openssl-dev", "ccache"
     )
-    .run_commands( # https://developer.nvidia.com/cuda-gpus
+    .run_commands(
+        "git clone --depth 1 --branch b7193 https://github.com/ggml-org/llama.cpp",
+        force_build=False,
+    )
+    .run_commands("nvidia-smi", gpu=GPU)
+    .run_commands(  # https://developer.nvidia.com/cuda-gpus
         'cmake llama.cpp -B llama.cpp/build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DLLAMA_CURL=ON -DCMAKE_CUDA_ARCHITECTURES="75;80;86;89;90;100"',
-        gpu=GPU 
+        gpu=GPU,
     )
     .run_commands(  # this one takes a few minutes!
         "cmake --build llama.cpp/build --config Release -j --clean-first --target llama-server",
-        gpu=GPU
+        gpu=GPU,
     )
     .run_commands("cp llama.cpp/build/bin/llama-* llama.cpp")
     .entrypoint([])  # remove NVIDIA base container entrypoint
@@ -72,16 +75,16 @@ llama_image = (
             "LLAMA_ARG_UBATCH": UBATCH,
             "LLAMA_ARG_N_GPU_LAYERS": N_GPU_LAYERS,
             "LLAMA_ARG_N_PARALLEL": PARALLEL,
-            "LLAMA_API_KEY": API_KEY
+            "LLAMA_API_KEY": API_KEY,
         }
     )
-    
 )
 app = modal.App("llama-cpp-server")
 
+
 @app.function(
     image=llama_image,
-    gpu=GPU,  
+    gpu=GPU,
     volumes={MODEL_DIR: model_volume},
     timeout=60 * 5,  # 5 minutes max input runtime
     scaledown_window=300,  # Timeout after 5 minutes of inactivity.
@@ -91,7 +94,8 @@ app = modal.App("llama-cpp-server")
 @modal.web_server(port=8080, startup_timeout=180)
 def serve():
     import subprocess
-    cmd = [ "/llama.cpp/llama-server --port 8080 --host 0.0.0.0" ]
+
+    cmd = ["/llama.cpp/llama-server --port 8080 --host 0.0.0.0"]
     print(cmd)
     subprocess.Popen(" ".join(cmd), shell=True)
     print("Serving llama.cpp API on port 8080")
