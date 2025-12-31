a2af503b17059fd7907f013513380543b9aeb6e3
jnraptor <jnraptor@gmail.com>
2025-12-31 14:34:18 +0800

    Update llama.cpp

--- llama-serve-modal.py
+++ llama-serve-modal.py
@@ -50,7 +50,7 @@ llama_image = (
         "git", "build-essential", "cmake", "curl", "libcurl4-openssl-dev", "ccache"
     )
     .run_commands(
-        "git clone --depth 1 --branch b7502 https://github.com/ggml-org/llama.cpp",
+        "git clone --depth 1 --branch b7588 https://github.com/ggml-org/llama.cpp",
         force_build=False,
     )
     .run_commands("nvidia-smi", gpu=GPU)
